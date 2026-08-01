6a1e7d7f351207ff5394eb12747f22f3d31e897c
Ashraf <ashrafunnisa135@gmail.com>
2026-08-02 07:07:12 +1000

    add delete operation

--- storage/localStorage.js
+++ storage/localStorage.js
@@ -1,32 +1,47 @@
-export let transactionHistory =  [];
+export let transactionHistory = [];
 
 export function loadDataFromLocalStorage() {
-    transactionHistory = JSON.parse(localStorage.getItem("Transaction-History")) || [];
-};
+  transactionHistory =
+    JSON.parse(localStorage.getItem("Transaction-History")) || [];
+}
+
+export function saveDataToLocalStorage() {
+  localStorage.setItem("Transaction-History", JSON.stringify(transactionHistory));
+}
 
 export function saveTransaction(transaction) {
-    transactionHistory.push(transaction);
-    localStorage.setItem("Transaction-History", JSON.stringify(transactionHistory));
+  transactionHistory.push(transaction);
+  saveDataToLocalStorage();
+}
+
+export function deleteTransaction(id) {
+    const index = transactionHistory.findIndex(transaction => transaction.id === id);
+    if( index > -1) {
+        transactionHistory.splice(index, 1);
+    }
+            saveDataToLocalStorage();
+            renderTransactions();
 };
 
 export function renderTransactions() {
-    const tbody = document.getElementById("transactionTable");
+  const tbody = document.getElementById("transactionTable");
 
-    tbody.innerHTML = "";   
-    transactionHistory.forEach((transaction) => {
-        const newRow = document.createElement("tr");
+  tbody.innerHTML = "";
+  transactionHistory.forEach((transaction) => {
+    const newRow = document.createElement("tr");
 
-        newRow.innerHTML = `
+    newRow.innerHTML = `
                 <td>${transaction.title}</td>
                 <td>${transaction.category}</td>
                 <td>${transaction.date}</td>
                 <td>${transaction.amount}</td>
                 <td>
-                    <button type="button" id="deleteBtn">
+                    <button type="button" id="deleteBtn" data-id=${transaction.id} class="delete-btn">
                         <i class="fa-regular fa-trash-can"></i>
                     </button>
                 </td>
         `;
-        tbody.appendChild(newRow);
-    });
+    tbody.appendChild(newRow);
+  });
 }
+
